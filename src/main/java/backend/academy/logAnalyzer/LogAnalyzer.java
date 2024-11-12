1b2e11825bf35fe8934f85f2a396cd3df7d6251f
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-11-12 22:41:54 +0300

    feat: add calculating percentile and considering logs in the given time interval

--- src/main/java/backend/academy/logAnalyzer/LogAnalyzer.java
+++ src/main/java/backend/academy/logAnalyzer/LogAnalyzer.java
@@ -18,6 +18,7 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -29,14 +30,17 @@ import java.util.stream.Stream;
 
 public class LogAnalyzer {
     private static final String UNDEFINED = "None";
+    private static final int TIMESTAMP_ID = 3;
     private static final int RESOURCE_ID = 4;
     private static final int RESPONSE_CODE_ID = 5;
     private static final int RESPONSE_SIZE_ID = 6;
+    private static final double PERCENTILE = 0.95;
     private static final Pattern LOG_PATTERN = Pattern.compile(
         "^(\\S+) - (\\S+) \\[([^]]+)] "
             + "\"([^\"]+)\" (\\d{3}) (\\d+) "
             + "\"([^\"]*)\" \"([^\"]*)\"$"
     );
+    private static final DateTimeFormatter LOG_DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MMM/yyyy:HH:mm:ss Z");
     private final PrintStream output;
 
     public LogAnalyzer(PrintStream output) {
@@ -46,7 +50,7 @@ public class LogAnalyzer {
     public void analyze(String path, LocalDateTime fromDate, LocalDateTime toDate, String format) {
         List<LogData> logsData = new ArrayList<>();
         if (isValidURL(path)) {
-            try (Stream<LogData> stream = createStreamFromURL(path, output)) {
+            try (Stream<LogData> stream = createStreamFromURL(path, output, fromDate, toDate)) {
                 if (stream != null) {
                     logsData.addAll(stream.toList());
                 }
@@ -55,7 +59,7 @@ public class LogAnalyzer {
             try {
                 List<Path> logFiles = getMatchingFiles(path);
                 logFiles.stream()
-                    .flatMap(LogAnalyzer::parseFileToStream)
+                    .flatMap(line -> parseFileToStream(line, fromDate, toDate))
                     .forEach(logsData::add);
             } catch (IOException e) {
                 output.println("IO exception!");
@@ -65,7 +69,7 @@ public class LogAnalyzer {
     }
 
     private static List<Path> getMatchingFiles(String userPathPattern) throws IOException {
-        String basePath = "src/main/resources/";  // Base directory for resources
+        String basePath = "src/main/resources/";
         String fullPathPattern = basePath + userPathPattern;
 
         List<Path> logFiles = new ArrayList<>();
@@ -83,28 +87,38 @@ public class LogAnalyzer {
         return logFiles;
     }
 
-    private static Stream<LogData> parseFileToStream(Path filePath) {
+    private static Stream<LogData> parseFileToStream(Path filePath, LocalDateTime fromDate, LocalDateTime toDate) {
         try {
             return Files.lines(filePath)
-                .map(LogAnalyzer::parseLineToLogData)
+                .map(line -> parseLineToLogData(line, fromDate, toDate))
                 .filter(Objects::nonNull);
         } catch (IOException e) {
             return Stream.empty();
         }
     }
 
-    private static LogData parseLineToLogData(String line) {
+    private static LogData parseLineToLogData(String line, LocalDateTime from, LocalDateTime to) {
         Matcher matcher = LOG_PATTERN.matcher(line);
         if (matcher.find()) {
-            String resource = matcher.group(RESOURCE_ID).split(" ")[1];
-            String responseCode = matcher.group(RESPONSE_CODE_ID);
-            long responseSize = Long.parseLong(matcher.group(RESPONSE_SIZE_ID));
-            return new LogData(resource, responseCode, responseSize);
+            String currentLogTime = matcher.group(TIMESTAMP_ID);
+            LocalDateTime timestamp = LocalDateTime.parse(currentLogTime, LOG_DATE_FORMATTER);
+
+            if ((from == null || !timestamp.isBefore(from)) && (to == null || !timestamp.isAfter(to))) {
+                String resource = matcher.group(RESOURCE_ID).split(" ")[1];
+                String responseCode = matcher.group(RESPONSE_CODE_ID);
+                long responseSize = Long.parseLong(matcher.group(RESPONSE_SIZE_ID));
+                return new LogData(resource, responseCode, responseSize);
+            }
         }
         return null;
     }
 
-    private static Stream<LogData> createStreamFromURL(String urlString, PrintStream output) {
+    private static Stream<LogData> createStreamFromURL(
+        String urlString,
+        PrintStream output,
+        LocalDateTime fromDate,
+        LocalDateTime toDate
+    ) {
         try {
             URL url = new URI(urlString).toURL();
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -112,11 +126,25 @@ public class LogAnalyzer {
 
             BufferedReader reader = new BufferedReader(
                 new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            return reader.lines().map(LogAnalyzer::parseLineToLogData).filter(Objects::nonNull);
+            return reader.lines()
+                .map(line -> parseLineToLogData(line, fromDate, toDate)).filter(Objects::nonNull);
         } catch (IOException | URISyntaxException e) {
             output.println("Error fetching or reading from URL: " + urlString);
             return Stream.empty();
         }
+    }
+
+    private static double calculatePercentile(List<LogData> logDataList) {
+        List<Long> responseSizes = logDataList.stream()
+            .map(LogData::responseSize)
+            .sorted()
+            .toList();
+
+        if (!responseSizes.isEmpty()) {
+            int index = (int) Math.ceil(PERCENTILE * (responseSizes.size() - 1));
+            return responseSizes.get(index);
+        }
+        return 0;
     }
 
     private void processLogData(List<LogData> logDataList, String format) {
@@ -141,7 +169,10 @@ public class LogAnalyzer {
             .average()
             .orElse(0.0);
 
-        printResults(totalRequests, mostRequestedResource, mostFrequentResponseCode, avgResponseSize, output);
+        double percentile95 = calculatePercentile(logDataList);
+
+        printResults(totalRequests, mostRequestedResource, mostFrequentResponseCode, avgResponseSize, percentile95,
+            output);
     }
 
     private void printResults(
@@ -149,12 +180,14 @@ public class LogAnalyzer {
         String mostRequestedResource,
         String mostCommonResponseCode,
         double averageResponseSize,
+        double percentile95,
         PrintStream output
     ) {
         output.println("Total Requests: " + totalRequests);
         output.println("Most Requested Resource: " + mostRequestedResource);
         output.println("Most Common Response Code: " + mostCommonResponseCode);
         output.println("Average Response Size: " + averageResponseSize);
+        output.printf("95th Percentile Response Size: %.2f bytes%n", percentile95);
     }
 
     private boolean isValidURL(String url) {
