67cd37919331dd651c52497413deb019e0d7959b
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-11-17 20:48:12 +0300

    feat: add program execution in the Main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,21 @@
 package backend.academy;
 
+import backend.academy.logAnalyzer.CollectedData;
+import backend.academy.logAnalyzer.InputParser;
+import backend.academy.logAnalyzer.LogAnalyzer;
+import backend.academy.logAnalyzer.LogReportGenerator;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        InputParser inputParser = new InputParser(System.out, System.in);
+        inputParser.parseData();
+        LogAnalyzer logAnalyzer = new LogAnalyzer(System.out);
+        CollectedData parsedData =
+            logAnalyzer.analyze(inputParser.path(), inputParser.from(), inputParser.to(), inputParser.agentValue());
+        LogReportGenerator logReport =
+            new LogReportGenerator(inputParser.format(), inputParser.from(), inputParser.to());
+        logReport.generateLog(logAnalyzer.processedFiles(), parsedData, System.out);
     }
 }
