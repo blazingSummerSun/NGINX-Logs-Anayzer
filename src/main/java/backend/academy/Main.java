ae56dfa3289208280727a3567bfbe065aad11666
Adel Gaznanov <a.gaznanov@innopolis.university>
2024-11-18 10:20:32 +0300

    doc: write javadoc for Main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -6,8 +6,27 @@ import backend.academy.logAnalyzer.LogAnalyzer;
 import backend.academy.logAnalyzer.LogReportGenerator;
 import lombok.experimental.UtilityClass;
 
+/**
+ * <p>The Main class where the main program starts execution. This class is responsible for initializing
+ * the log analysis and report generation processes.</p>
+ *
+ * <h2>Input Example:</h2>
+ * <pre>
+ * analyzer --path logs/[file_name] --from [ISO8601_date_pattern]
+ * --to [ISO8601_date_pattern] --filter-field agent
+ * --filter-value "[filter_pattern]" --format markdown
+ * </pre>
+ * After the program execution, the generated log file will be in the project directory.
+ */
 @UtilityClass
 public class Main {
+    /**
+     * The main method which serves as the entry point for the program.
+     * It initializes the InputParser, LogAnalyzer, and LogReportGenerator,
+     * and coordinates the whole log processing workflow.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         InputParser inputParser = new InputParser(System.out, System.in);
         inputParser.parseData();
